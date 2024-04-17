0385bd8ea319304e0ce6ed2c2c29b69b792d742e
minseong99 <rlacjfks123@gmail.com>
2024-04-17 22:45:09 +0900

    fix:단어 길이 확인 로직 수정
    -경고 메세지가 여러번 나오는것을 막기 위해 조건문을 주어 문제를 해결하였습니다.

--- frontend/js/makeGame.js
+++ frontend/js/makeGame.js
@@ -13,25 +13,32 @@ const findSubject = () => {
 
 const checkWordLength = (result) => {
   const word_list = document.querySelector("#word_list");
+  const div = document.querySelector("#alert-message");
+  if (div != null) word_list.removeChild(div);
   if (result.length < 10) {
     const alert = document.createElement("div");
     alert.id = "alert-message";
     alert.innerText = "At least 10 words are required";
     word_list.appendChild(alert);
-    return;
+    return false;
   }
+  return true;
 };
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const words = formData.getAll("word");
+  const title = formData.get("title");
+  const description = formData.get("description");
 
   let subject = findSubject(); // on 또는 null
 
   // filtering
   result = words.filter((word) => word.length > 0);
-  checkWordLength(result);
+  if (!checkWordLength(result)) {
+    return;
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
