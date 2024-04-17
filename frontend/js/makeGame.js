32b086d7cad1cd5068d03d95e1e7595cb34ec8d9
minseong99 <rlacjfks123@gmail.com>
2024-04-17 21:46:49 +0900

    feat:주제 얻는 로직 추가
    -check box가 체크된 주제를 얻는 함수findSubject를 만들었습니다.

--- frontend/js/makeGame.js
+++ frontend/js/makeGame.js
@@ -1,5 +1,16 @@
 const form = document.querySelector("#form-block");
 
+const findSubject = () => {
+  const checkBoxs = document.getElementsByName("check");
+  let subject;
+  checkBoxs.forEach((box) => {
+    if (box.checked === true) {
+      subject = box.id;
+    }
+  });
+  return subject;
+};
+
 const checkWordLength = (result) => {
   const word_list = document.querySelector("#word_list");
   if (result.length < 10) {
@@ -15,6 +26,8 @@ const handleSubmit = (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const words = formData.getAll("word");
+
+  let subject = findSubject(); // on 또는 null
 
   // filtering
   result = words.filter((word) => word.length > 0);
