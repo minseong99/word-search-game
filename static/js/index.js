3b166d5671ba71b5b21e60b577cc0b4bcc280667
minseong99 <rlacjfks123@gmail.com>
2024-04-18 21:26:49 +0900

    feat:form데이터 서버에 보내기
    -클라이언트에서 form에 있는 데이터를 서버에 POST요청으로 보냈습니다.

--- static/js/index.js
+++ static/js/index.js
@@ -15,7 +15,7 @@ const checkWordLength = (result) => {
   const word_list = document.querySelector("#word_list");
   const div = document.querySelector("#alert-message");
   if (div != null) word_list.removeChild(div);
-  if (result.length < 10) {
+  if (result.length < 2) {
     const alert = document.createElement("div");
     alert.id = "alert-message";
     alert.innerText = "At least 10 words are required";
@@ -23,6 +23,23 @@ const checkWordLength = (result) => {
     return false;
   }
   return true;
+};
+
+const requestMakeGame = async (title, description, wordList, subject) => {
+  const res = await fetch("/game", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      description,
+      wordList,
+      subject,
+    }),
+  });
+  const jsonRes = await res.json();
+  console.log(jsonRes);
 };
 
 const handleSubmit = (event) => {
@@ -35,10 +52,12 @@ const handleSubmit = (event) => {
   let subject = findSubject(); // on 또는 null
 
   // filtering
-  result = words.filter((word) => word.length > 0);
-  if (!checkWordLength(result)) {
+  wordList = words.filter((word) => word.length > 0);
+  if (!checkWordLength(wordList)) {
     return;
   }
+
+  requestMakeGame(title, description, wordList, subject);
 };
 
 form.addEventListener("submit", handleSubmit);
