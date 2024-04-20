64145db41bca409161c79133b007778b9b6d0e6a
minseong99 <rlacjfks123@gmail.com>
2024-04-20 21:04:30 +0900

    feat: 게임 형식 db 저장 로직 완성!
    -게임의 제목, 설명, 주제등을 games에 저장하는 로직을 만들었습니다.

--- python/sql.py
+++ python/sql.py
@@ -8,6 +8,7 @@ cur = con.cursor()
 
 def insert_word_location(word, start_row, start_col, direction):
     
+    #id는 나중에 실제 사용자 id로 바꿀것임 
     cur.execute(f"""
                 INSERT INTO words(word, start_row, start_col, direction)
                 VALUES ('{word}',{start_row}, {start_col}, '{direction}')
@@ -15,6 +16,20 @@ def insert_word_location(word, start_row, start_col, direction):
     con.commit()
     print("success")
     return 
+
+
+def insert_game_info(title, description, subject):
+    
+    cur = con.cursor()
+    #id는 나중에 실제 사용자 id로 바꿀것임 
+    cur.execute(f"""
+                INSERT INTO games(title, description, subject)
+                VALUES ('{title}', '{description}', '{subject}')
+                """)
+    
+    con.commit()
+    print("games_success")
+    return
 
 
 
